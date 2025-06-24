04e5255e189c51025b0930d94adcb28b0700bfe3
MalathiSelvaraj <103201231+MalathiSelvaraj@users.noreply.github.com>
2025-06-24 09:17:29 +0530

    Add files via upload

---
+++ employee-service/src/main/java/com/wipro/employee/EmployeeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.wipro.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone");
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+}
